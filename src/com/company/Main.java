b869ee1d95b1dbae83a22cb577ec20ed6ca22b56
AlexMT96 <44590084+AlexMT96@users.noreply.github.com>
2019-09-01 16:15:02 -0700

    program can write to a text file but reading from it still has bugs, it break when reading from the file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,25 @@ import java.io.*;
 import java.nio.file.Files;
 import java.util.Scanner;
 
+
+
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+
+
+
+    public static void main(String[] args) throws IOException {
 	//This creates a directory and fills the info
         String PATH = "C:/Users/alexm/Desktop/data/";
+
+        //beer information
+        String name;
+        String brand;
+        String location;
+        int volume;
+        int alcohol;
+        int quantity;
+
 
         File dir = new File("C:/Users/alexm/Desktop/data/");
 
@@ -20,7 +34,7 @@ public class Main {
         {
             System.out.println("Directory does not exist, creating it...");
             new File("C:/Users/alexm/Desktop/data/").mkdirs();
-            File stockfile = new File("C:/Users/alexm/Desktop/data/inventory.csv");
+            File stockfile = new File("C:/Users/alexm/Desktop/data/inventory.txt");
             try {
                 stockfile.createNewFile();
             } catch (IOException e)
@@ -31,54 +45,72 @@ public class Main {
             System.out.println("stock file" + stockfile.getPath() + " created ");
         }
 
-        System.out.println("Insert the next data:");
+
         Scanner sc = new Scanner(System.in);
+        int option = sc.nextInt();
 
-        String name;
-        String brand;
-        String location;
-        int volume;
-        int alcohol;
-        int quantity;
-        System.out.print("Name: ");
-        name = sc.nextLine();
-        System.out.print("\nBrand: ");
-        brand = sc.nextLine();
-        System.out.print("\nLocation: ");
-        location = sc.nextLine();;
-        System.out.print("\nVolume: ");
-        volume = sc.nextInt();
-        System.out.print("\nAlcohol %: ");
-        alcohol =sc.nextInt();
-        System.out.print("\nHow many: ");
-        quantity = sc.nextInt();
 
-        try {
-            PrintWriter writer = new PrintWriter("C:/Users/alexm/Desktop/data/inventory.csv");
-            StringBuilder sb = new StringBuilder();
-            sb.append(name);
-            sb.append(',');
-            sb.append(brand);
-            sb.append(',');
-            sb.append(location);
-            sb.append(',');
-            sb.append(volume);
-            sb.append(',');
-            sb.append(alcohol);
-            sb.append(',');
-            sb.append(quantity);
-            sb.append('\n');
 
-            writer.write(sb.toString());
+        //input data and write
+        if(option == 1){
 
-            System.out.println("done!");
 
-        }catch (FileNotFoundException e){
-            System.out.println(e.getMessage());
+            System.out.println("Insert the next data:");
 
+
+            System.out.print("Name: ");
+            name = sc.nextLine();
+            System.out.print("\nBrand: ");
+            brand = sc.nextLine();
+            System.out.print("\nLocation: ");
+            location = sc.nextLine();;
+            System.out.print("\nVolume: ");
+            volume = sc.nextInt();
+            System.out.print("\nAlcohol %: ");
+            alcohol =sc.nextInt();
+            System.out.print("\nHow many: ");
+            quantity = sc.nextInt();
+
+
+            String info = name + ":" + brand + ":" + location + ":" + volume + ":" + alcohol + ":" + quantity;
+
+            File inventory = new File("C:/Users/alexm/Desktop/data/inventory.txt");
+            FileWriter fileWriter = new FileWriter(inventory, true);
+            fileWriter.write(info);
+            fileWriter.write("\r\n");
+            fileWriter.flush();
+            fileWriter.close();
         }
 
+        //read data from the file
+        if(option == 2){
+            Scanner read = new Scanner("C:/Users/alexm/Desktop/data/inventory.txt");
+            read.useDelimiter(":");
+            while(read.hasNext()){
+                name = read.next();
+                System.out.println("Name: " +  name);
 
+                brand = read.next();
+                System.out.println("Brand: " +  brand);
+
+                location = read.next();
+                System.out.println("Location: " +  location);
+
+                volume = read.nextInt();
+                alcohol = read.nextInt();
+                quantity = read.nextInt();
+
+                System.out.println("Volume: " +  volume);
+                System.out.println("Alcohol %: " +  alcohol);
+                System.out.println("Quantity: " +  quantity);
+
+
+
+
+
+            }
+
+        }
 
 
     }
